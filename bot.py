e55893c77c11196451ef4d97fb5e1e825b537fba
marenzie <50057378+marenzie@users.noreply.github.com>
2020-07-18 10:54:21 -0500

    Laying framework for admin menu

--- bot.py
+++ bot.py
@@ -416,7 +416,7 @@ async def forbid(ctx, keyword, *args):
 @bot.command(name='admin', help='Allows setup of various commands and permissions in the bot. Done through DMs.')
 async def admin(message):
     if message.author != bot.user:
-        if message.author.dm_channel.id is not None:
+        if message.author.dm_channel is not None and message.channel is not None:
             response = 'This command cannot be called straight from DMs. Please use this command in the server you ' \
                        'want to configure options for.'
             await message.send(response)
