fa0c79a2baf2f316d674ae628ee4f644373dae0b
marenzie <50057378+marenzie@users.noreply.github.com>
2020-08-09 10:22:59 -0500

    Laying groundwork for spank command

--- bot.py
+++ bot.py
@@ -418,7 +418,7 @@ async def forbid(ctx, keyword, *args):
 
 
 @bot.command(name='spank', help='Adds a spank to the user, can be used for many purposes!')
-async def spank(ctx, number = 1):
+async def spank(ctx, number: int = 1):
     async with ctx.channel.typing():
         if hasattr(ctx, 'raw_mentions'):
             await ctx.send("You mentioned someone, but I don't know how to spank yet.")
