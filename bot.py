ab69f59cc1d0daa82b8b23a9116c380a8280f410
marenzie <50057378+marenzie@users.noreply.github.com>
2020-08-09 10:58:23 -0500

    Laying groundwork for spank command

--- bot.py
+++ bot.py
@@ -422,7 +422,47 @@ async def spank(ctx):
     async with ctx.channel.typing():
         if hasattr(ctx.message, 'raw_mentions'):
             await ctx.send("You asked me to spank someone, but I don't know how to spank yet.")
-            await ctx.send(str(ctx.message.raw_mentions))
+            for member_id in ctx.message.raw_mentions:
+                guild = ctx.author.guild
+
+                # Has this person been spanked before?
+                Boogerball.cursor.execute("SELECT ID FROM spanks WHERE ID = %(ID)s AND guild = %(guild)s",
+                                          {'ID': str(member_id), 'guild': str(guild.id)})
+                check = Boogerball.cursor.fetchall()
+
+                # Make a new row if this is the first time this person has been spanked.
+                if len(check) == 0:
+                    Boogerball.cursor.execute("INSERT INTO spanks (ID, guild, spanks) VALUES "
+                                              "(%(ID)s, %(guild)s, 1)",
+                                              {'ID': str(member_id), 'guild': str(guild.id)})
+
+                # Add to the spanks count if they've been here before.
+                else:
+                    Boogerball.cursor.execute("UPDATE spanks SET spanks = spanks + 1 "
+                                              "WHERE ID = %(ID)s AND guild = %(guild)s",
+                                              {'ID': str(member_id), 'guild': str(guild.id)})
+
+                # Now get how many spanks they have in total.
+                Boogerball.cursor.execute("SELECT spanks FROM spanks WHERE ID = %(ID)s",
+                                          {'ID': str(member_id)})
+                stats = Boogerball.cursor.fetchone()
+                spanks = stats[0]
+
+                # Let's have a few funny phrases to play with.
+                list_spank_phrases = [
+                    "Lo! The spank bell doth toll for <@!{}>! Bask in the sound of a hand smacking the ass!"
+                    " It has rung {} time{}!".format(str(member_id), str(spanks), check_plural(spanks)),
+                    "Soups on! One spank for <@!{}>! Comin' right up! It's been served for them () time{}!"
+                    .format(str(member_id), str(spanks), check_plural(spanks)),
+                    "THWACK! My favorite sound... And right now it's coming from <@!{}>'s ass!"
+                    " I've heard it {} time{} so far!".format(str(member_id), str(spanks), check_plural(spanks)),
+                    "M-M-M-MONSTER SPANK! GET DISCIPLINED <@!{}>! YOU'VE BEEN TAUGHT THIS LESSON {} TIME{}!"
+                    .format(str(member_id), str(spanks), check_plural(spanks))
+                ]
+
+                # Inform the victim of their spank!
+                await ctx.send(list_spank_phrases[random.randint(0, (len(list_spank_phrases) - 1))])
+
         else:
             await ctx.send("You didn't mention anyone! How will I ever know where to direct this frustration?!")
 
