3b24b4b1f4cca6a22ec84819f1a9bc88f657a25a
marenzie <50057378+marenzie@users.noreply.github.com>
2020-10-24 09:36:41 -0500

    Changing the presence to 'Listening to' instead of 'Playing'

--- bot.py
+++ bot.py
@@ -236,7 +236,7 @@ async def on_ready():
     print('\n')
     print(f'{bot.user.name} has connected to Discord!')
     tenor_token = str(sys.argv[2])
-    await bot.change_presence(activity=discord.Activity(name='Listening to $help'))
+    await bot.change_presence(activity=discord.Activity(name='$help', ActivityType='listening'))
 
 
 @bot.event
