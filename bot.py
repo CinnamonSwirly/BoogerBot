eccf929d6d464f39c4903d8c794e1755d79cd80d
marenzie <50057378+marenzie@users.noreply.github.com>
2020-10-24 19:13:41 -0500

    Lay the activity_check command one step at a time...

--- bot.py
+++ bot.py
@@ -330,7 +330,7 @@ async def test_history(ctx):
     history = []
     for channel in channels:
         if type(channel) is discord.TextChannel:
-            history.append(await channel.history(after=then).get())
+            history.append(await channel.history().get())
 
     if history is None:
         response = "Nothing to return"
