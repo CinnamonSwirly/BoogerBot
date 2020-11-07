4cf896e34fb89b6a6498ca5921e053371e65ad08
marenzie <50057378+marenzie@users.noreply.github.com>
2020-11-07 09:16:05 -0600

    Lay the activity_check command one step at a time...

--- bot.py
+++ bot.py
@@ -339,7 +339,7 @@ async def on_member_join(member):
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    await reaction.message.channel.send(reaction.emoji.name)
+    await reaction.message.channel.send(reaction.emoji)
 
 
 @bot.command(name='test_history', hidden=True)
